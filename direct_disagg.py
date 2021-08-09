5f85760f1788dc6696523a2fd3fbd1dc82f20c4f
Yiyang Lab PC <yiyangw@umich.edu>
2021-08-08 22:41:34 -0400

    08082021C1

--- direct_disagg.py
+++ direct_disagg.py
@@ -6,7 +6,6 @@ from python_tsp.exact import solve_tsp_dynamic_programming
 def direct_disagg(
     Route_D_agg: dict[int, list[tuple[int, int, int, int, int, int]]],
     OD_d: dict[int, dict[str, int]],
-    OD_r: dict[int, int],
     Driver: "np.ndarray[np.int64]",
     tau_disagg: "np.ndarray[np.int64]",
     tau_agg: "np.ndarray[np.int64]",
@@ -15,9 +14,10 @@ def direct_disagg(
     disagg_2_agg_id: dict[int, int],
     config: dict[str, any],
     fixed_route_D: dict = None,
-):
+    OD_r: dict[int, int] = None,
+) -> dict[int, list[tuple[int, int, int, int]]]:
     """
-    Direct disaggregation algorithm:
+    Direct disaggregation algorithm (only for flex routes):
     Input:
     Route_D_agg -> {d: list((t1, t2, s1, s2, n1, n2))}
     OD_d -> {d: {"O": o, "D": d}}, disaggregated network
@@ -34,22 +34,24 @@ def direct_disagg(
     S = config["S_disagg"]
 
     # set of drivers, drivers' origins and destinations of disagg network
-    D = set(OD_d.keys())
+    # not include current fixed routes
+    D = set(OD_d.keys()) if not config["FIXED_ROUTE"] else set(config["driver_set"])
 
-    # disagg nodes need to be visited to serve passengers
+    # DEPRECIATED: disagg nodes need to be visited to serve passengers
     S_r = set()
-    for OD_dictionary in OD_r.values():
-        S_r.add(OD_dictionary["O"])
-        S_r.add(OD_dictionary["D"])
+    if OD_r:
+        for OD_dictionary in OD_r.values():
+            S_r.add(OD_dictionary["O"])
+            S_r.add(OD_dictionary["D"])
 
     # time window for each driver. If repeated tour mode,
     # TW_d should be the time window for the first single tour
     TW_d = {d: Driver[i, 3] - Driver[i, 2] for i, d in enumerate(Driver[:, 0])}
     # each driver only register its first tour info
-    for d in D:
-        Route_D_agg[d] = [
-            station for station in Route_D_agg[d] if station[1] <= TW_d[d]
-        ]
+    Route_D_agg = {
+        d: [station for station in route if station[1] <= TW_d[d]]
+        for d, route in Route_D_agg.items()
+    }
     # node set for each driver, disagg
     S_d = {
         d: set(
@@ -99,7 +101,7 @@ def direct_disagg(
     df_route_agg = pd.DataFrame()
     for d, route in Route_D_agg.items():
         df_route_agg = pd.concat(
-            [df_route_agg, pd.DataFrame({d: route})], ignore_index=True, axis=1
+            [df_route_agg, pd.DataFrame({d: route})], ignore_index=False, axis=1
         )
     os = {d: OD_d[d]["O"] for d in D}  # origin sub-node within each super-node
     ds = dict()  # end sub-node within each super-node
@@ -141,8 +143,9 @@ def direct_disagg(
                 else:
                     # check if the current super-node is the hub or not
                     if index > 0:
-                        # if not the first super-node, os[d] will be the closest sub-node
-                        # to the ds[d] from previous partition
+                        # if the current super-node is not a hub node,
+                        # and if the current node is not the first super-node, os[d] will be the
+                        # closest sub-node to the ds[d] from previous partition
                         if s_super not in H_d[d]:
                             os[d] = station[
                                 np.argmin(tau_disagg[ds[d], s] for s in station)
@@ -158,15 +161,27 @@ def direct_disagg(
                             t_hub[(s_super, d, row[d][0])] = t_d[d]
 
                             for (h_temp, d_temp, t_temp) in t_hub.keys():
+                                # case 1: 当前driver提前其他driver到达hub
                                 if (
                                     d_temp != d
                                     and h_temp == s_super
                                     and t_temp <= row[d][0]
                                     and t_hub[h_temp, d_temp, t_temp] > t_d[d]
                                 ):
-                                    # amount of time the driver d_temp need to wait
+                                    # amount of time the driver d need to wait
                                     delta = t_hub[h_temp, d_temp, t_temp] - t_d[d]
-                                    # TODO!
+                                    # add waiting time to t_d[d]
+                                    t_d[d] += delta
+                                # case 2: 当前driver迟于某driver到达hub
+                                elif (
+                                    d_temp != d
+                                    and h_temp == s_super
+                                    and t_temp >= row[d][0]
+                                    and t_hub[t_temp, d_temp, t_temp] < t_d[d]
+                                ):
+                                    # amount of time the driver d need to wait
+                                    delta = t_hub[h_temp, d_temp, t_temp] - t_d[d]
+                                    # add waiting time for d_temp
                                     Route_D_disagg[d_temp] = [
                                         (t1, t2, s1, s2)
                                         if t1 < t_hub[h_temp, d_temp, t_temp]
@@ -202,3 +217,72 @@ def direct_disagg(
                         t_d[d] += tau_disagg[s1, s2]
 
                     Route_D_disagg[d] += sub_station
+
+    return Route_D_disagg
+
+
+if __name__ == "__main__":
+    import yaml
+    import pickle
+    import os
+    import csv
+    from utils import load_tau_disagg, load_neighbor_disagg, load_FR_m2m_gc
+
+    test_driver_set = {
+        0,
+        1,
+        2,
+        3,
+    }
+
+    with open("config_m2m_gc.yaml", "r+") as fopen:
+        config = yaml.load(fopen, Loader=yaml.FullLoader)
+
+    config["T_post_processing"] = 70
+    config["m2m_output_loc"] = r"many2many_output\\results\\route_disagg_test\\"
+    config["m2m_data_loc"] = r"many2many_data\\FR_and_1_ATT_SPTT_GC\\"
+    config["figure_pth"] = r"many2many_output\\figure\\route_disagg_test\\"
+
+    if not os.path.exists(config["figure_pth"]):
+        os.makedirs(config["figure_pth"])
+
+    result_loc = config["m2m_output_loc"]
+    if not os.path.exists(result_loc):
+        os.makedirs(result_loc)
+
+    Route_D = pickle.load(open("Data\\debug\\Route_D.p", "rb"))
+
+    for d in Route_D.keys():
+        route_filename = result_loc + r"route_{}_agg.csv".format(d)
+        with open(route_filename, "w+", newline="", encoding="utf-8") as csvfile:
+            csv_out = csv.writer(csvfile)
+            csv_out.writerow(["t1", "t2", "s1", "s2", "n1", "n2"])
+            for row in Route_D[d]:
+                csv_out.writerow(row)
+    agg_2_disagg_id = pickle.load(open("Data\\debug\\agg_2_disagg_id.p", "rb"))
+    disagg_2_agg_id = {
+        n: partition for partition, nodes in agg_2_disagg_id.items() for n in nodes
+    }
+    config["S"] = len(agg_2_disagg_id)
+
+    OD_d = pd.read_csv(
+        config["m2m_data_loc"] + "Driver_OD_disagg.csv", index_col=["ID"]
+    ).to_dict("index")
+
+    # Load fixed routed infomation
+    _, FR, _ = load_FR_m2m_gc(
+        agg_2_disagg_id, disagg_2_agg_id, config
+    )  # aggregated routes
+    # FR = {
+    #     d: np.array([[s[0], agg_2_disagg_id[s[1]][0]] for s in route])
+    #     for d, route in FR.items()
+    #     if d in test_driver_set
+    # }
+    Route_D = {d: route for d, route in Route_D.items() if d in test_driver_set}
+    tau, tau2 = load_tau_disagg(config)
+    ctr = load_neighbor_disagg(config)
+
+    Route_D = direct_disagg(
+        Route_D,
+        OD_d,
+    )
