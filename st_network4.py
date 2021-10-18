b62cc285285a06fbf7251b9bbb1c840b882fb448
Yiyang Lab PC <yiyangw@umich.edu>
2021-10-18 10:17:36 -0400

    Update st_network4.py

--- st_network4.py
+++ st_network4.py
@@ -486,10 +486,11 @@ def Many2Many(Rider, Driver, tau, tau2, ctr, V, config, fixed_route_D=None):
     if FIXED_ROUTE and fixed_route_D != None:
         for (d, n1, n2) in DL_d:
             if (d, n1, n2) in FRL_d:
+                stop1, stop2 = n1 % S, n2 % S
                 for m in range(num_tour[d]):
                     n1_foo, n2_foo = (
-                        n1 + (m * Duration_d[d]) * S,
-                        n2 + (m * Duration_d[d]) * S,
+                        stop1 + (m * Duration_d[d]) * S,
+                        stop2 + (m * Duration_d[d]) * S,
                     )
                     if (d, n1_foo, n2_foo) in DL_d and (d, n1_foo, n2_foo) not in seen:
                         x[d, n1_foo, n2_foo].LB = 1
